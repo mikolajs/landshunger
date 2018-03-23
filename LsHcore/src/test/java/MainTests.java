6536b56ffd42e35b63619f51c848f14c3d26e189
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2018-03-23 14:11:09 +0100

    gradle project

---
+++ LsHcore/src/test/java/MainTests.java
@@ -0,0 +1,12 @@
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTests {
+	@Test
+	void justAnExample() {
+        System.out.println("This test method should be run");
+        assertEquals(4 , 1 + 1);
+    }
+}
