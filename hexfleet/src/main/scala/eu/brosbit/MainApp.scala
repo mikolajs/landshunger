11a9ce545928c00923d8077920a86ff94f0d8144
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2019-10-31 11:01:27 +0100

    fix errors

--- hexfleet/src/main/scala/eu/brosbit/MainApp.scala
+++ hexfleet/src/main/scala/eu/brosbit/MainApp.scala
@@ -8,6 +8,9 @@ import com.scalawarrior.scalajs.createjs._
 
 object MainApp  {
 
+  var started = true
+  var time = 0L
+
   def main(args:Array[String]): Unit = {
     println("Starting 'hexfleet'...")
 
@@ -23,12 +26,22 @@ object MainApp  {
     val h = 720 //stage.canvas.height
 
 
-    val circle = new Shape()
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50)
-    circle.x = 100
-    circle.y = 100
-    stage.addChild(circle)
+    val background = new Shape();
+    background.graphics.beginFill("#444444")
+    background.graphics.drawRect(0,0, w, h)
+    stage.addChild(background)
+    Ticker.setFPS(60)
+    Ticker.addEventListener("tick", stage)
+    stage.addEventListener("tick", handleTick)
     stage.update();
+  }
+
+  def handleTick(event:Any) =  {
+    if (!(event.asInstanceOf[Event].paused) && started) {
+       time += 1
+      if(time % 600 == 0) println(time)
+    }
+    true
   }
 
 }
