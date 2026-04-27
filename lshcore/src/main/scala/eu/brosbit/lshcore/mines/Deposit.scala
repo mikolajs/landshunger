c8513bf8fa2bb677055158ecb484acb379fc11d8
Administrator <mikolajsochacki@gmail.com>
2026-04-27 15:28:59 +0200

    build deck mine

--- lshcore/src/main/scala/eu/brosbit/lshcore/mines/Deposit.scala
+++ lshcore/src/main/scala/eu/brosbit/lshcore/mines/Deposit.scala
@@ -4,8 +4,10 @@ class Deposit(aType:String):
   val (area, depth, aParam, bParam) = randomDeposit()
   println(s"area $area, depth: $depth, A: $aParam, B: $bParam")
   val workConst = 100
-  var workDeck = 0
-  var deckValue = 0
+  val woodConst = 0.3 // how many wood per unit work
+  var canDig = false
+  var deckWork = 0
+  var deckValue = 0.0
   var step = 0
   def mkDig(work:Int) = 
     if step > depth then
@@ -16,24 +18,44 @@ class Deposit(aType:String):
       deckValue = 0
       workR -= workDeck
       workDeck = 0
+      canDig = false
+      deckSize = 0
       (deposit, work - workDeck)
     else
       val deposit = deckValue*(work/workDeck)
       workDeck -= work
+      if workDeck == 0 then 
+        canDig = false
+        deckSize = 0
       (deposit, 0)
 
-  def canBuildDeck():Boolean = workDeck == 0 && step < depth
+  def canBuildDeck():Boolean =  !canDig && step < depth
 
   def buildDeck(work:Int, wood:Int) =
-    if step >= depth then
-      (work, wood)
+    if !canBuildDeck() then (work, wood)
     else
-      step += 1
-      workDeck = area*workConst/10
-      deckValue = (-aParam*step*step + bParam*step).toInt
-      println(s"$step, $deckValue")
-      (work, wood) 
+      val availibleWoodWork = scala.math.round(wood / woodConst).toInt
+      val doWork = if availibleWoodWork >= work then work else availibleWoodWork
+      if doWork >= deckWork then
+        wood -= scala.math.ceil(deckWork*woodConst).toInt
+        work -= deckWork
+        deckWork = 0
+        mkStartDig()
+        (work, wood)
+      else
+        deckWork -= doWork
+        work -= doWork
+        wood -= scala.math.ceil(doWork*woodConst).toInt
+        (work, wood)
+        
 
+      
+  private def mkStartDig() =
+    canDig = true
+    val deckRich = (-aParam*step*step + bParam*step).toDouble
+    deckWork = area*workConst
+    deckValue = deckRich/deckWork.toDouble
+    
   private def randomDeposit() = 
     import scala.util.Random
     val rand = Random()
@@ -55,4 +77,6 @@ def main():Unit =
     iron += mine.deckValue
   println(s"all iron values $iron")
   
+
+main()
 
