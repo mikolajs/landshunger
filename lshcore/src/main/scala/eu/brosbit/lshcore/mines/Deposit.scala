c52b60c30d682a4fc7fe3a474f7050fb9b56adce
Administrator <mikolajsochacki@gmail.com>
2026-06-16 11:57:07 +0200

    deposit fix

--- lshcore/src/main/scala/eu/brosbit/lshcore/mines/Deposit.scala
+++ lshcore/src/main/scala/eu/brosbit/lshcore/mines/Deposit.scala
@@ -3,52 +3,57 @@
 class Deposit(aType:String, reachConst:Double):
   val (area, depth, aParam, bParam) = randomDeposit()
   println(s"area $area, depth: $depth, A: $aParam, B: $bParam")
-  val workConst = 100
-  val woodConst = 0.15 // how many wood per unit work
+  import Deposit.*
   var canDig = false
   var deckWork = 0
   var deckValue = 0.0
   var depDiged = 0.0
+  var woodGather = 0
   var step = 0
+  mkStartBuild()
+  //return diged deposit and used work
   def mkDig(work:Int):(Int, Int) = 
     if step > depth then
       (0, work)
     else if  work > deckWork then
-      var workR = work
       depDiged += deckValue 
       val deposit = depDiged.toInt 
       depDiged -= depDiged.toInt
-      deckValue = 0.0
-      workR -= deckWork 
-      deckWork= 0
-      canDig = false
-      (deposit, work - deckWork)
+      val workR = deckWork 
+      mkStartBuild()
+      (deposit, workR )
     else
-      val deposit = deckValue*(work/deckWork)
+      val dv = deckValue*(work.toDouble/deckWork.toDouble)
+      //print(s"dv = $dv, deckValue = $deckValue")
+      depDiged += dv
+      deckValue -= dv
+      val deposit = depDiged.toInt
+      depDiged -= deposit
       deckWork -= work
       if deckWork == 0 then 
         canDig = false
         deckValue = 0
-      (deposit.toInt, 0)
+      (deposit, work)
 
   def canBuildDeck():Boolean =  !canDig && step < depth
-
+  
+  //return used work and used wood
   def buildDeck(work:Int, wood:Int) =
-    if !canBuildDeck() then (work, wood)
+    if !canBuildDeck() then (0, 0)
     else
-      val availibleWoodWork = scala.math.round(wood / woodConst).toInt
+      val availibleWoodWork = scala.math.floor(wood / woodConst).toInt
       val doWork = if availibleWoodWork >= work then work else availibleWoodWork
-      var woodR = wood
+      //println(s"doWork  = $doWork, $work, $wood, $deckWork")
       if doWork >= deckWork then
-        woodR -= scala.math.ceil(deckWork*woodConst).toInt
+        val woodR = scala.math.floor(deckWork*woodConst).toInt
         val workR = deckWork
         deckWork = 0
         mkStartDig()
-        (work-workR, woodR)
+        (workR, woodR)
       else
         deckWork -= doWork
-        woodR -= scala.math.ceil(doWork*woodConst).toInt
-        (work-doWork, woodR)
+        val woodR = scala.math.floor(doWork*woodConst).toInt
+        (doWork, woodR)
         
   def maxPeopleWork = area
       
@@ -56,6 +61,11 @@ class Deposit(aType:String, reachConst:Double):
     canDig = true
     deckValue = (-aParam*step*step + bParam*step).toDouble
     deckWork = area*workConst
+
+  private def mkStartBuild() =
+    canDig = false
+    deckValue = 0
+    deckWork = area*workConst 
     step += 1
     
   private def randomDeposit() = 
@@ -68,27 +78,35 @@ class Deposit(aType:String, reachConst:Double):
     val AA:Double = BB/depthTmp
     (areaTmp, depthTmp, AA, BB) 
 
+object Deposit:
+  val workConst = 20
+  val woodConst = 0.15 // how many wood per unit work !change after change workConst
+
+/*
 @main
 def main():Unit = 
   val mine = Deposit("iron", 0.95)
   var iron = 0
   var wood = 0 
-  var work = 0
+  var workD = 0
+  var workB = 0
   import scala.util.Random
   val r = Random()
   while mine.step < mine.depth do
+    println(s"step: ${mine.step}")
     while !mine.canDig do
-      val woodS = r.nextInt(40)+10
-      val workS = r.nextInt(200)+100
+      val woodS = r.nextInt(20)+10
+      val workS = r.nextInt(100)+500
       val (rWork, rWood) = mine.buildDeck(workS, woodS)
-      wood += (woodS - rWood)
-      work += (workS - rWork)
-      println(s"build deck $wood, $work, ${mine.step}")
+      wood += rWood
+      workB += rWork
+      println(s"build deck $wood, $workB")
     while mine.canDig do
-      val workS = r.nextInt(200)+100
+      val workS = r.nextInt(100)+50
       val (dep, rWork) = mine.mkDig(workS)
-      work += (workS - rWork)
-      iron += dep.toInt
-      println(s"mk dig $iron, $work")
+      workD += rWork
+      iron += dep
+      //println(s"mk dig dep=$dep, work=$rWork")
 
-  println(s"all iron values $iron works: $work, timber: $wood")
+  println(s"all iron values $iron works: dig $workD build $workB, timber: $wood")
+*/
